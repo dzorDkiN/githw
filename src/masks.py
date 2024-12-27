bb0e37db038069fb4cd8d36cadfbf3283ac80ef8
Никита Дроздов <nikitadrozdov@MacBook-Pro-Nikita.local>
2024-12-27 21:05:42 +0400

    get_date

--- src/masks.py
+++ src/masks.py
@@ -5,17 +5,16 @@ def get_mask_card_number(card_num: Union[str]) -> Union[str]:
 
     """Функция скрывает часть введенного номера карты"""
 
-    return f"{card_num [:4]} {card_num [4:6]}** **** {card_num [-4:]}"
+    return f"{card_num [:-12]} {card_num [-12:-10]}** **** {card_num [-4:]}"
 
 
-print(get_mask_card_number("7000792289606361"))
 
 
-def get_mask_account(score_bank: Union[str]) -> Union[str]:
+
+def get_mask_account(card_num: Union[str]) -> Union[str]:
 
     """Функция скрывает большую чассть номера счета"""
 
-    return f"**{score_bank[-4:]}"
+    return f"{card_num[0:4]} **{card_num[-4:]}"
 
 
-print(get_mask_account("73654108430135874305"))
--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,17 @@
+from mypy.join import join_types
+
 from src.masks import get_mask_card_number,get_mask_account
 from typing import Union
 
-def mask_account_card():
-    pass
+def mask_account_card(card_num: Union[str]) -> Union[str]:
+    split_num = card_num.split()
+    if split_num[0].lower()=='счет':
+            new_split_num = ''.join(split_num)
+            return get_mask_account(new_split_num)
+    else:
+        return get_mask_card_number(card_num)
 
-
-
-def get_date():
-    pass
+def get_date(data:Union[str]) -> Union[str]:
+    split_data = data.split('T')
+    new_data = split_data[0].split('-')
+    return  f'{new_data[-1]}.{new_data[1]}.{new_data[0]}.'
